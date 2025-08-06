bd5278f6be8b1fb317dc740ac0c25476ca77232f
ONsUbhaM <debarpanchatterjee4@gmail.com>
2025-08-06 17:08:48 +0530

    PortFolio

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Content from "./components/content";
+import Navbar from "./components/navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Contact from "./components/contact";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={Content} />
+          <Route path='/about' Component={About} />
+          <Route path='/skills&projects' Component={Skills} />
+          <Route path='/contact' Component={Contact} />
+          <Route path='*' component={() => <h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <div className='flex justify-center'>
+        <h1 className='flex'>
+          made by{" "}
+          <span className='font-bold ml-2 uppercase cursor-pointer text-stone-600 hover:text-purple-600'>
+            <a href='https://github.com/ONsUbhaM' target='_blank'>
+              Debarpan Chatterjee 
+            </a>
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projectData.js
@@ -0,0 +1,39 @@
+import protfolioImg from "./components/icons/portfolio.png";
+import recipeAppImg from "./components/icons/recipeApp.png";
+import foodOrderAppImg from "./components/icons/SpaNFood.png";
+import BookMyStayImg from "./components/icons/BookMyStay.jpg";
+
+export const projectData = [
+  // {
+  //   id: 1,
+  //   name: "Portfolio",
+  //   image: protfolioImg,
+  //   stack: ["React", "Tailwind CSS"],
+  //   githubLink: "https://github.com/samarpansarkar/portfolio",
+  //   liveLink: "https://portfolio-samarpan.vercel.app/",
+  // },
+  // {
+  //   id: 2,
+  //   name: "RecipeApp",
+  //   image: recipeAppImg,
+  //   stack: ["MERN stack"],
+  //   githubLink: "https://github.com/samarpansarkar/MERN-recipe-app",
+  //   liveLink: "https://mern-recipe-app-silk.vercel.app/",
+  // },
+  {
+    id: 3,
+    name: "FoodOrderApp",
+    image: foodOrderAppImg,
+    stack: ["MERN stack"],
+    githubLink: "https://github.com/samarpansarkar/MERN-foodApp",
+    liveLink: "https://mern-food-app-one.vercel.app/",
+  },
+  // {
+  //   id: 4,
+  //   name: "BookMyStay",
+  //   image: BookMyStayImg,
+  //   stack: ["MERN stack"],
+  //   githubLink: "https://github.com/samarpansarkar/BookMyStay",
+  //   liveLink: "https://book-my-stay-samarpan-sarkars-projects.vercel.app/",
+  // }
+];
